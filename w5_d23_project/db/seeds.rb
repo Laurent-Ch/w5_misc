9e905a2159f847ee9fd4b833555133dc3ca72023
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-27 17:33:13 +0200

    working version 1

---
+++ w5_d23_project/db/seeds.rb
@@ -0,0 +1,13 @@
+Movie.destroy_all
+ActiveRecord::Base.connection.execute('ALTER TABLE table_name AUTO_INCREMENT = 1')
+
+
+
+=begin
+require 'faker'
+
+genre_array = ["action", "horreur", "comédie", "drame"]
+100.times do
+  fake_movie = Movie.create!(name: Faker::Movie;title, year: rand(1920..2000), genre: genre_array[rand(0..3)], synopsis: Faker::Lorem.sentences(number: 10), director: Faker::Name.name, allocine_rating: rand(0.0..5.0).round(1), my_rating: nil, already_seen: false) 
+end
+=end
